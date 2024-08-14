9fd00d6a002d150d1b08b330bacc672ee32ca283
itay2341 <itay2341@gmail.com>
2024-08-14 11:45:01 +0300

    server

--- server/server.js
+++ server/server.js
@@ -5,17 +5,13 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-// Cors configuration - Allows requests from localhost:4200
 const corsOptions = {
   origin: "http://localhost:4200",
   optionsSuccessStatus: 204,
   methods: "GET",
 };
 
-// Use cors middleware
 app.use(cors(corsOptions));
-
-// Use express.json() middleware to parse JSON bodies of requests
 app.use(express.json());
 
 app.get("/products", (req, res) => {
@@ -26,7 +22,6 @@ app.get("/products", (req, res) => {
       res.status(500).send("Internal Server Error");
       return;
     }
-
     const jsonData = JSON.parse(data);
     res.status(200).json(jsonData);
   });
